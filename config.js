ec60eedfeedcbd4673614d5f8af248d3c66cb426
Amovane <amovane@163.com>
2022-04-15 23:33:06 +0800

    Update addresses

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const contractAddress = 0x5fbdb2315678afecb367f032d93f642f64180aa3;
-export const ownerAddress = 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266;
+export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
