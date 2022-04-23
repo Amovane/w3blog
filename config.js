729146c6e51c28beb26df10e50c34e15485687e9
Amovane <amovane@163.com>
2022-04-23 16:45:32 +0800

    Deploy to polygon testnet

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+export const contractAddress = "0x162cA43Ea875678eE71Df38303d3849A4A56fa94";
+export const ownerAddress = "0x2e05333a18264795612a7Bd38DdD95212D9197e9";
